5688811c3f8e0f69ae5ae086498429f541f41855
Pratham Patil <prathamlpatil04@gmail.com>
2025-08-14 00:11:08 +0530

    logo

--- src/components/navigation/Logo.tsx
+++ src/components/navigation/Logo.tsx
@@ -13,7 +13,7 @@ const Logo: React.FC<LogoProps> = ({ scrollToSection }) => {
         onClick={() => scrollToSection('#hero')}
         className="text-xl font-bold gradient-text font-mono"
       >
-        {`<${personalInfo.personal.nickname} />`}
+        {`<${personalInfo.personal.nickname}/>`}
       </button>
     </div>
   );
